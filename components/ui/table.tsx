5ee6229ed8d143ccec2380571ed3fa2146dfb554
phidtech <phidtechnology@gmail.com>
2026-03-15 18:30:08 +0300

    feat: responsive design - table scroll, mobile dialogs, touch inputs, global CSS fixes

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -3,8 +3,8 @@ import * as React from "react";
 
 const Table = React.forwardRef<HTMLTableElement, React.HTMLAttributes<HTMLTableElement>>(
   ({ className, ...props }, ref) => (
-    <div className="relative w-full overflow-auto">
-      <table ref={ref} className={cn("w-full caption-bottom text-sm", className)} {...props} />
+    <div className="relative w-full overflow-x-auto" style={{ WebkitOverflowScrolling: "touch" }}>
+      <table ref={ref} className={cn("w-full caption-bottom text-sm min-w-[600px]", className)} {...props} />
     </div>
   )
 );
